31f9ee81cd99687381173f9aebdc2df27a45c466
Moosa <moosamir68@gmail.com>
2019-05-04 21:22:07 +0430

    Initial Commit

---
+++ MMMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMMovie
+//
+//  Created by iOS Developer on 5/4/19.
+//  Copyright © 2019 MMMovie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
